a73f763f7f44107dea105b04a067578631f151c3
max-jellycat <max@jellycat.fr>
2020-10-07 02:57:03 +0200

    button layout

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -5,6 +5,8 @@ import '../widgets/box.dart';
 import '../widgets/icon_content.dart';
 
 const activeCardColour = Color(0xFF1D1E33);
+const bottomButtonColor = Color(0xFFEB1555);
+const bottomButtonHeight = 80.0;
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,49 +20,62 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Box(
-                      color: activeCardColour,
-                      child: IconContent(
-                        icon: FontAwesomeIcons.mars,
-                        label: 'Male',
-                      ),
-                    ),
-                    Box(
-                        color: activeCardColour,
-                        child: IconContent(
-                          icon: FontAwesomeIcons.venus,
-                          label: 'Female',
-                        )),
-                  ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Box(
+                  color: activeCardColour,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'Male',
+                  ),
                 ),
-              ),
-              Box(
-                color: activeCardColour,
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Box(
-                      color: activeCardColour,
-                    ),
-                    Box(
-                      color: activeCardColour,
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                Box(
+                    color: activeCardColour,
+                    child: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'Female',
+                    )),
+              ],
+            ),
           ),
-        ),
+          Box(
+            color: activeCardColour,
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Box(
+                  color: activeCardColour,
+                ),
+                Box(
+                  color: activeCardColour,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomButtonColor,
+            width: double.infinity,
+            height: bottomButtonHeight,
+            margin: EdgeInsets.only(top: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'CALCULATE',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
